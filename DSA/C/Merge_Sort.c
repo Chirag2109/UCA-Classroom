2bf5b58a7ccaa8331a784038a67e869693f992ec
Chirag2109 <122337662+Chirag2109@users.noreply.github.com>
2024-08-22 12:30:19 +0530

    Update Merge_Sort.c
    added code snippet to the file but driver code is not yet written.

--- DSA/C/Merge_Sort.c
+++ DSA/C/Merge_Sort.c
@@ -1,1 +1,27 @@
+void merge(int arr[], int aux[], int left, int mid, int right) {
+    for(int k = left; k <= right; k++)
+      aux[k] = arr[k];
+  
+    int i = left;
+    int j = mid + 1;
+    for(int k = left; k <= right; k++) {
+        if(i > mid) arr[k] = aux[j++];
+        else if(j > right) arr[k] = aux[i++];
+        else if(aux[i] <= aux[j]) arr[k] = aux[i++];
+        else arr[k] = aux[j++];
+    }
+}
 
+void merge_sort_r(int arr[], int aux[], int left, int right) {
+    if(left >= right) return;
+    int mid = (left + right) / 2;
+    merge_sort_r(arr, aux, left, mid);
+    merge_sort_r(arr, aux, mid+1, right);
+    merge(arr, aux, left, mid, right);
+}
+
+void merge_sort(int arr[], int n) {
+    int * aux = (int *) malloc(n * sizeof(int));
+    merge_sort_r(arr, aux, 0, n-1);
+    free(aux);
+}
