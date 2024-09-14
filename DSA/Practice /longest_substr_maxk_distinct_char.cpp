83f2033934de0da947a3d5870c1101e705a45483
Chirag2109 <122337662+Chirag2109@users.noreply.github.com>
2024-09-15 00:14:41 +0530

    Update and rename longest_substr.cpp to longest_substr_maxk_distinct_char.cpp

--- DSA/Practice /longest_substr_maxk_distinct_char.cpp
+++ DSA/Practice /longest_substr_maxk_distinct_char.cpp
@@ -2,13 +2,13 @@
 #include <unordered_map>
 using namespace std;
 
-int longest_substr_max2_distinct_char(string s)
+int longest_substr_maxk_distinct_char(string s, int k)
 {
     unordered_map<char, int> m;
     int n = s.length();
     int ptr1 = 0;
     int ans = 0;
-    if(n <= 2)
+    if(n <= k)
     {
         ans = n;
     }
@@ -17,13 +17,13 @@ int longest_substr_max2_distinct_char(string s)
         for(int ptr2 = 0; ptr2 < n; ptr2++)
         {
             m[s[ptr2]]++;
-            if(m.size() <= 2)
+            if(m.size() <= k)
             {
                 ans = max(ans, ptr2-ptr1+1);
             }
             else
             {
-                while(m.size() > 2)
+                while(m.size() > k)
                 {
                     if(m[s[ptr1]] > 1)
                         m[s[ptr1++]]--;
@@ -46,9 +46,11 @@ int main()
     while(t--)
     {
         string s;
+        int k;
 
         cin >> s;
-        cout << longest_substr_max2_distinct_char(s) << "\n";
+        cin >> k;
+        cout << longest_substr_max2_distinct_char(s, k) << "\n";
     }
 
     return 0;
